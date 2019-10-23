c23a4994438a2cca1ceab109b749c3d37d748a59
Yash Patel <31024657+yashlo@users.noreply.github.com>
2019-10-23 15:33:13 +0530

    Add files via upload

---
+++ T11.py
@@ -0,0 +1,65 @@
+import mysql.connector
+print("====>>>>Stock Management System<<<<====");
+def connect():
+      connection = mysql.connector.connect(host="localhost",database='stock',user="root",password="");
+      return connection;
+def menu1():
+    print("Enter your choice");
+    print(" 1-show all stoke");
+    print(" 2-Insert stoke");
+    print(" 3- Selling stoke");
+    print(" 4- search record");
+def display(connection):
+    cursor=connection.cursor();
+    cursor.execute("select * from stock");
+    row=cursor.fetchall();
+    for x in row:
+        print(x);
+def insert(connection):
+    cur = connection.cursor();
+    prod=input("Enter the item model");
+    cate=input("Enter the item categouri");
+    qty=int(input("Enter the quantity"));
+    price=int(input("Enter the price"));
+    print ("writing to db");
+    sql="INSERT INTO stock VALUES ('%s','%s','%d','%d')";
+    args=(prod,cate,qty,price);
+    res=cur.execute(sql%args);
+    connection.commit();
+def sell(connection):
+    cur = connection.cursor();
+    prod=input("Enter the model item number that user want to buy");
+    qty1=int(input("Enter the quantity that user want to buy"));
+    sql="update  stock set qty = qty - '%d'  where modelid='%s' and qty >= '%d'";
+    args=(qty1,prod,qty1);
+    cur.execute(sql%args);
+    connection.commit();
+def search(connection):
+    prod=input("Enter the item model that you want to seaech");
+    cursor=connection.cursor();
+    sql="select * from stock where modelid='%s'";
+    args=(prod);
+    res=cursor.execute(sql%args);
+    row=cursor.fetchall();
+    for x in row:
+        print(x);
+    #display();
+connection = connect();
+menu1();
+ch=int(input("Enter your choice"));
+while(ch<=5):
+      menu1();
+      if(ch==1):
+          display(connection);
+          break;
+      elif(ch==2):
+          insert(connection);
+          display(connection);
+          break;
+      elif(ch==3):
+          sell(connection);
+          display(connection);
+          break;
+      elif(ch==4):
+            search(connection);
+            break;
